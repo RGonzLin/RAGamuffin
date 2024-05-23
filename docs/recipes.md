966b91692d8e31eebd79c18a56a9b9b8ff24d832
rodgl1994 <r.gonz.lin@gmail.com>
2024-05-23 03:07:01 +0200

    Added sample docs

---
+++ docs/recipes.md
@@ -0,0 +1,48 @@
+# Cheesecake
+
+## Ingredients (serves 6)
+- 600 g of cream cheese
+- 4 small eggs
+- 200 g of sugar
+- 20 g of flour
+- 250 ml of heavy cream
+
+## Instructions for making the cheesecake
+1. Crack the eggs into a bowl and beat them with a manual whisk.
+2. Add the sugar and flour, and mix slightly.
+3. Add the room temperature cream cheese (do not beat) and continue mixing with the whisk.
+4. Finally, pour in the heavy cream and mix until the batter is smooth.
+5. Line an 18 cm diameter springform pan with parchment paper or baking paper.
+6. Pour the mixture into the pan and bake at 200°C for 40-45 minutes (with the oven preheated).
+7. Remove the cheesecake from the oven and let it rest (3-4 hours) until it cools completely before unmolding.
+8. Unmold the cake and carefully remove the paper without breaking it.
+9. Serve the cheesecake and cut it into slices.
+
+# Ham Croquettes
+
+## Ingredients
+- 200 g of Serrano ham
+- 1 scallion
+- 1 clove of garlic
+- 100 g of flour
+- 100 g of butter (or 100 ml of extra virgin olive oil)
+- 1 liter of milk
+- Parsley
+- Flour (for coating)
+- 3 beaten eggs
+- Bread crumbs
+- Extra virgin olive oil (for frying)
+
+## Instructions
+1. First, cut the butter into cubes and melt it in a saucepan over heat.
+2. Finely chop the scallion and garlic clove and add them to the saucepan. Sauté everything for 5-6 minutes.
+3. Then add the chopped ham and sauté it.
+4. Once the ham is cooked, add the flour and sauté for a couple of minutes to cook the flour through.
+5. Gradually pour in the milk, stirring constantly.
+6. Cook the béchamel sauce for about 20 minutes, stirring continuously. When done, sprinkle with chopped parsley and mix.
+7. Spread the mixture onto a platter. Butter the surface to prevent a crust from forming, or cover with plastic wrap and let it cool completely (it can be left overnight in the refrigerator).
+8. Once the mixture is cold, cut or take small portions and shape them into your preferred croquette shape: rectangular, oval, square, round, etc.
+9. Prepare three plates: one with flour, one with the beaten eggs, and another with breadcrumbs. Coat the croquettes first in flour, then dip them in egg, and finally cover them with breadcrumbs.
+10. Fry the croquettes in a pot or skillet with plenty of hot olive oil, making sure they brown on all sides.
+11. Once cooked, remove the croquettes from the skillet and place them on a plate lined with paper towels to drain excess oil.
+12. Serve the ham croquettes immediately to maintain their texture.
---
+++ docs/scripts/raggamuffin.py
@@ -0,0 +1,250 @@
+# RAGgamuffin by Rodrigo González Linares
+
+import ollama
+import os
+import PyPDF2
+import faiss
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+
+def extract_text_from_txt(txt_path):
+    
+    try:
+        with open(txt_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except:
+        # Ignore the document if an error occurs
+        return None
+
+def extract_text_from_directory(directory):
+
+    documents = {}
+    for root, _, files in os.walk(directory):
+        for file in files:
+            path = os.path.join(root, file)
+            text = None
+            if file.endswith(('.txt', '.md', '.py', '.sh', '.js')): # Add more extensions if needed!
+                text = extract_text_from_txt(path)
+            # Add text to dictionary only if text extraction was successful
+            if text:
+                documents[path] = text
+
+    return documents
+
+def get_youtube_id(url):
+    
+    # Finding the start index of the video ID
+    start_index = url.find('v=') + 2
+
+    return url[start_index:]
+
+def extract_text_from_webpage(url):
+
+    # Check if the URL is from YouTube
+    if "youtube.com" in url:
+        # Extracting the video id
+        video_id = get_youtube_id(url)
+        # Fetching the transcript
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        # Combining all text parts from the transcript
+        text = ' '.join([entry['text'] for entry in transcript])
+
+    else:
+        # Send request
+        request = requests.get(url)
+        # Parse the HTML content
+        soup = BeautifulSoup(request.text, 'html.parser')
+        # Extract all text from the webpage
+        text = soup.get_text()
+
+    return text
+
+
+
+def main():
+
+    default = input("Use default LLM (llama3), embedding model (all-minilm), and path (docs/)? (y/n): ")
+
+    if default == "y":
+        llm = "llama3"
+        embedding_model = "all-minilm"
+        docu_path = "docs/"
+    
+    else:
+        # Get the user to setup the models and the documents path
+        llm = input("LLM: ")
+        embedding_model = input("Embedding model: ")
+        docu_path = input("Path to documents : ")
+
+    print("Extracting texts")
+
+    # Extract text from the documents
+    texts = extract_text_from_directory(docu_path)
+
+    print("Embedding documents...")
+
+    # Embed the documents
+    embeddings = {}
+    for document, text in texts.items(): 
+        embeddings[document] = ollama.embeddings(model=embedding_model, 
+                                                    prompt=text)['embedding']
+        
+    # Delete the texts to save memory
+    del texts
+
+    print(f"{len(embeddings)} documents embedded!")
+
+    print("Indexing documents...")
+
+    # Convert the dictionary values to a 2D numpy array
+    doc_names = list(embeddings.keys())
+    doc_embeddings = np.array(list(embeddings.values()), dtype=np.float32)
+
+    # Normalize the embeddings to use cosine similarity
+    faiss.normalize_L2(doc_embeddings)
+
+    # Create a index
+    index = faiss.IndexFlatIP(doc_embeddings.shape[1]) 
+
+    # Add vectors to the index
+    index.add(doc_embeddings)
+
+    print("Done! Let's RAGgamuff!")
+
+    # Initialize parameters
+    rag = True # RAG is on by default
+    k_docs = 1 # Number of documents to retrieve
+    min_score = 0.0 # Minimum similarity score to retrieve a document, orthogonal by default
+    rag_docs = True # Show the documents retrieved by RAG if True
+    rag_flag = True # Flag to momentarily turn off RAG
+    history = [] # Chat history
+
+    while True:
+
+        print("")
+        user_input = input(">> ")
+
+        # Exit chat
+        if user_input == "/exit":
+            print("Bye!")
+            break
+
+        # Turn RAG off
+        if user_input == "/ragoff":
+            rag = False
+            print("RAG OFF")
+            continue
+
+        # Turn RAG on
+        if user_input == "/ragon":
+            rag = True
+            print("RAG ON")
+            continue
+
+        # Change the number of documents to retrieve
+        if user_input == "/kdocs":
+            k_docs = int(input("Number of documents to retrieve: "))
+            print(f"Now the topr {k_docs} will be retrieved for RAG")
+            continue
+
+        # Change the minimum similarity score to retrieve a document
+        if user_input == "/minscore":
+            min_score = float(input("Minimum similarity score to retrieve a document: "))
+            print(f"Now the minimum similarity score is {min_score}")
+            continue
+
+        # Stop showing the documents retrieved by RAG
+        if user_input == "ragdocsoff":
+            rag_docs = False
+            print("Name of RAGged documents will not be shown")
+            continue
+
+        # Show the documents retrieved by RAG
+        if user_input == "ragdocson":
+            rag_docs = True
+            print("Name of RAGged documents will be shown")
+            continue
+
+        # Specify system prompt
+        if user_input == "/system":
+
+            user_input = input("Add system prompt: ")
+
+            history.append({
+                    "role": "system",
+                    "content": user_input,
+                    })
+            continue
+
+        # Chat with the LLM
+        else:
+
+            # Query a webpage
+            if user_input == "/interwebs":
+                url = input("URL: ")
+                user_input = input("What do you want to know? >> ")
+                web_text = extract_text_from_webpage(url)
+                augmented_input = user_input + '\n' + web_text
+                history.append({
+                        "role": "user",
+                        "content": augmented_input,
+                        })
+                # Momentarily turn off RAG
+                rag_flag = False
+
+            
+            if rag and rag_flag:
+                
+                # Embed the user input and normalize it
+                query = ollama.embeddings(model = embedding_model, prompt = user_input)['embedding']
+
+                # Check the query is not empty
+                if query:
+
+                    # Process the query
+                    query = np.array([query], dtype=np.float32)
+                    faiss.normalize_L2(query)
+
+                    # Search for the most similar documents
+                    distances, indices = index.search(query, k_docs)
+
+                    # Filter out documents with similarity scores below the threshold
+                    indices = indices[0][distances[0] > min_score]
+
+                    # Retrieve the texts of the most similar documents
+                    similar_doc_names = [doc_names[i] for i in indices]
+                    if rag_docs:
+                        print(f'RAGged doc(s): {similar_doc_names}')
+                    # Concatenate the texts of the most similar documents spacing them with a newline
+                    similar_docs_text = '\n'.join([extract_text_from_txt(doc) for doc in similar_doc_names])
+                    # Concatenate the query and the text of the most similar documents
+                    user_input = user_input + '\n' + similar_docs_text
+
+            # If a weebpage was queried, skip this step
+            if rag_flag:
+                # Append the user input to the history
+                history.append({
+                    "role": "user",
+                    "content": user_input,
+                    })
+            # Turn on the rag flag in case it was turned off
+            rag_flag = True
+                
+            # Get the response with the LLM
+            response = ollama.chat(model = llm, messages = history)
+            print(response['message']['content'])
+            print("")
+
+            # Append the response to the history
+            history.append({
+                "role": "assistant",
+                "content": response['message']['content']
+                    })
+                    
+
+if __name__ == "__main__":
+    main()
